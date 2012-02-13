1d9ffd88218cef328946583326e7b15b1602f906
森 茂樹 <morishigeki@genta.local>
2012-02-13 20:20:16 +0900

    Initial Comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miniblog::Application.config.secret_token = '7c17f11fa6decc35b9b586746d5d7ff8119a4a101d286b3c72a519cc99016f497249ac0f44f313d25ef97228384116df54c9ea436ca7030a05dcf41bab109e25'
